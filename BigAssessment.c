65ae6214219e8e6f5b25abf2896a6efcf282bdc5
Ryan Stutchbury <ryan.stutch@gmail.com>
2019-04-19 10:35:21 +0000

    yeppo

--- BigAssessment.c
+++ BigAssessment.c
@@ -9,33 +9,38 @@ int main(){
     printf("first whats the phrase: ");
     scanf("%[^\n]s" ,str);
     scanf("%c",&burn);
-    printf("\n Now what do you want to do with it?\n a) Rotation Encryption\n b) Substitution Encryption\n c) Rotation Decryption\n d) Substitution Decryption\n Selection= ");
+    printf("\n Now what do you want to do with it?\n a) Known Rotation Encryption\n b) Known Substitution Encryption\n c) Known Rotation Decryption\n d) Known Substitution Decryption\n Selection= ");
     scanf("%c" ,&task);
-    switch(task){
-        case ('a'):
+switch(task){
+    case ('a'):
          printf("Key=  ");
-    scanf("%d",&key);
-    key=key%26;
+        scanf("%d",&key);
+        key=key%26;
     for(ind=0; str[ind]!='\0' ; ind++){
         
         if(str[ind] != 32){
-        
+         if(str[ind]>96 && str[ind]<123){
             if((str[ind]+key)<(92+key)){
               if (str[ind]+key>=91){
                 str[ind]=str[ind]-26;
         }
         }
+        str[ind] =str[ind]+key;
+        }
+        if(str[ind]<91 && str[ind]>64){
             if((str[ind]+key)>=(123)){
             str[ind]=str[ind]-26;
         }
-           str[ind] =str[ind]+key;
+            str[ind] =str[ind]+key;
+        }
+
      }   
     }
 
    printf("\nEncryption= %s\n", str);
  break;
         case ('b'):
-           printf("a b c d e f g h i j k l m n o p q r s t u v w x y z then type phrase\n");
+           printf("a b c d e f g h i j k l m n o p q r s t u v w x y z\n");
     scanf("%c",&a);
     scanf("%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",&a ,&b ,&c ,&d ,&e ,&f ,&g ,&h ,&i ,&j ,&k ,&l ,&m ,&n ,&o ,&p ,&q ,&r ,&s ,&t ,&u ,&v ,&w ,&x ,&y ,&z);
     for(ind=0; str[ind] != '\0'; ind++ ){
@@ -131,25 +136,113 @@ case('c'):
     scanf("%d",&key);
     key=key%26;
    for(ind=0; str[ind]!='\0' ; ind++){
-        if(str[ind] != 32){
-    
-            if(str[ind]>90){
+            if(str[ind]>96 && str[ind]<123){
             str[ind] =str[ind]-key;
                 if((str[ind])<(97)){
             str[ind]=str[ind]+26;
         }
         }
-        if(str[ind]<=90){
+        if(str[ind]<91 && str[ind]>64){
         str[ind] =str[ind]-key;    
           if((str[ind])<65){
             str[ind]=str[ind]+26;
-        }
         }
      }   
 }
    printf("\nDecrytion= %s\n",str);
     return 0;
+break;
+case ('d'):
+           printf("a b c d e f g h i j k l m n o p q r s t u v w x y z \n");
+    scanf("%c",&a);
+    scanf("%c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c %c",&a ,&b ,&c ,&d ,&e ,&f ,&g ,&h ,&i ,&j ,&k ,&l ,&m ,&n ,&o ,&p ,&q ,&r ,&s ,&t ,&u ,&v ,&w ,&x ,&y ,&z);
+    for(ind=0; str[ind] != '\0'; ind++ ){
+        switch(str[ind]){
+        case(32):
+            break;
+        case('a'):
+            str[ind]=a;
+            break;  
+        case('b'):
+            str[ind]=b;
+            break;
+        case('c'):
+            str[ind]=c;
+            break;
+        case('d'):
+            str[ind]=d;
+            break;
+        case('e'):
+            str[ind]=e;
+            break;
+        case('f'):
+            str[ind]=f;
+            break;
+        case('g'):
+            str[ind]=g;
+            break;
+        case('h'):
+            str[ind]=h;
+            break;
+        case('i'):
+            str[ind]=i;
+            break;
+        case('j'):
+            str[ind]=j;
+            break;
+        case('k'):
+            str[ind]=k;
+            break;
+        case('l'):
+            str[ind]=l;
+            break;
+        case('m'):
+            str[ind]=m;
+            break;
+        case('n'):
+            str[ind]=n;
+            break;
+        case('o'):
+            str[ind]=o;
+            break;
+        case('p'):
+            str[ind]=p;
+            break;
+        case('q'):
+            str[ind]=q;
+            break;
+        case('r'):
+            str[ind]=r;
+            break;  
+        case('s'):
+            str[ind]=s;
+            break;
+        case('t'):
+            str[ind]=t;
+            break;
+        case('u'):
+            str[ind]=u;
+            break;
+        case('v'):
+            str[ind]=v;
+            break;
+        case('w'):
+            str[ind]=w;
+            break;
+        case('x'):
+            str[ind]=x;
+            break;
+        case('y'):
+            str[ind]=y;
+            break;
+        case('z'):
+            str[ind]=z;
+            break;
+        }
 
+            }
+    printf("%s\n",str); 
+    break;
 }
 return 0;
 }
